bc07066da631681efcb4b5b81f687a24848a995c
thanayutPhi <thanayut.phi@outlook.com>
2019-09-10 11:03:19 +0700

    สร้างตัวแปร, สร้างเงื่อนไข

--- src/app/main/students/students.service.ts
+++ src/app/main/students/students.service.ts
@@ -1,13 +1,23 @@
 import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
 })
 export class StudentsService {
 
+  routeParam: any;
+
   constructor() { }
 
-  resolve(){
+  resolve(route: ActivatedRouteSnapshot) {
     console.log("resolve");
+    this.routeParam = route.params;
+    if (!this.routeParam.studentId) {
+      console.log("no");
+    } else {
+      console.log("yes");
+    }
+
   }
 }
