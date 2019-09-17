03a2d3bc0dceafc48ee89109c500074ce2166dc2
KrittinTch <petesuper256@gmail.com>
2019-09-17 18:23:44 +0700

    school form en/th

--- src/app/main/school/i18n/en.ts
+++ src/app/main/school/i18n/en.ts
@@ -2,7 +2,19 @@ export const locale = {
     lang: 'en',
     data: {
         'SCHOOL': {
-            'HELLO': 'Hello, World!'
+            'HELLO': 'Hello, World!',
+            'SCHOOLNAME': 'School Name',
+            'SUBDISTRIC': 'Sub Distric',
+            'DISTRIC': 'Distric',
+            'PROVINCE': 'Province',
+            'UNDER': 'Under',
+            'AREA': 'Area',
+            'REGISTRAR': 'Registrar',
+            'POSITION': 'Position',
+            'DIRECTOR': 'Director',
+            'POSITION2': 'Position',
+            'ADDSCHOOL': 'Add New School',
+            'SAVEBTN': 'Save'
         }
     }
 };
--- src/app/main/school/i18n/th.ts
+++ src/app/main/school/i18n/th.ts
@@ -2,7 +2,19 @@ export const locale = {
     lang: 'th',
     data: {
         'SCHOOL': {
-            'HELLO': 'สวัสดีชาวโลก!'
+            'HELLO': 'สวัสดีชาวโลก!',
+            'SCHOOLNAME': 'ชื่อโรงเรียน',
+            'SUBDISTRIC': 'ตำบล',
+            'DISTRIC': 'อำเภอ',
+            'PROVINCE': 'จังหวัด',
+            'UNDER': 'สังกัด',
+            'AREA': 'เขตพื้นที่',
+            'REGISTRAR': 'นายทะเบียน',
+            'POSITION': 'ตำแหน่ง',
+            'DIRECTOR': 'ผู้อำนวยการ',
+            'POSITION2': 'ตำแหน่ง',
+            'ADDSCHOOL': 'เพิ่มข้อมูลของโรงเรียน',
+            'SAVEBTN': 'บันทึกข้อมูล'
         }
     }
 };
