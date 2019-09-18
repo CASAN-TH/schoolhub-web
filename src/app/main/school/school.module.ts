8300fdc9a73abaa21b1830f4501dc93010e0b685
KrittinTch <petesuper256@gmail.com>
2019-09-18 11:44:15 +0700

    school authguard

--- src/app/main/school/school.module.ts
+++ src/app/main/school/school.module.ts
@@ -9,7 +9,8 @@ import { MatInputModule, MatIconModule, MatTableModule, MatPaginatorModule, MatM
 const routes = [
   {
       path     : '**',
-      component: SchoolComponent
+      component: SchoolComponent,
+      canActivate: [AuthenGuardService]
   }
 ];
 
