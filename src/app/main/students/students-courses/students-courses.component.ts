7decc001c527c9ecb827275514d14e298d7ec50d
thanayutPhi <thanayut.phi@outlook.com>
2019-09-27 10:11:43 +0700

    เริ่มทำ HTML  Form

--- src/app/main/students/students-courses/students-courses.component.ts
+++ src/app/main/students/students-courses/students-courses.component.ts
@@ -1,7 +1,11 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { StudentsService } from '../students.service';
-import { FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup } from '@angular/forms';
 import { fuseAnimations } from '@fuse/animations';
+import { Location } from '@angular/common';
+import { FuseTranslationLoaderService } from '@fuse/services/translation-loader.service';
+import { locale as english } from '../i18n/en';
+import { locale as thai } from '../i18n/th';
 
 @Component({
   selector: 'app-students-courses',
@@ -15,14 +19,66 @@ export class StudentsCoursesComponent implements OnInit {
   studentsForm: FormGroup;
   course: any;
 
-  constructor(private studentsService: StudentsService, ) { }
+  constructor(
+    private studentsService: StudentsService,
+    private _location: Location,
+    private formBuilder: FormBuilder,
+    private _fuseTranslationLoaderService: FuseTranslationLoaderService) { this._fuseTranslationLoaderService.loadTranslations(english, thai); }
 
   ngOnInit() {
-    // this.studentsService.onCoursesDataChanged.subscribe((res: any) => {
-    //   console.log(res)
-    //   this.course = res;
-      
-    // })
+    this.studentsService.onCoursesDataChanged.subscribe((res: any) => {
+      // console.log(res)
+      this.course = res;
+      if (!this.course) {
+        this.course = {
+          student_no: "",
+          title: "",
+          firstname: "",
+          lastname: "",
+          citizenid: "",
+          birthday: "",
+          birthmonth: "",
+          birthyear: "",
+          sex: "",
+          nationality: "",
+          religion: "",
+          fathername: "",
+          mothername: "",
+          attendencedate: "",
+          oldschool: "",
+          oldprovince: "",
+          lastclass: ""
+        }
+      }
+      this.studentsForm = this.createCoursesForm();
+    });
+  }
+
+  createCoursesForm(): FormGroup {
+    return this.formBuilder.group({
+      _id: [this.course._id],
+      student_no: [this.course.student_no],
+      title: [this.course.title],
+      firstname: [this.course.firstname],
+      lastname: [this.course.lastname],
+      citizenid: [this.course.citizenid],
+      birthday: [this.course.birthday],
+      birthmonth: [this.course.birthmonth],
+      birthyear: [this.course.birthyear],
+      sex: [this.course.sex],
+      nationality: [this.course.nationality],
+      religion: [this.course.religion],
+      fathername: [this.course.fathername],
+      mothername: [this.course.mothername],
+      attendencedate: [this.course.attendencedate],
+      oldschool: [this.course.oldschool],
+      oldprovince: [this.course.oldprovince],
+      lastclass: [this.course.lastclass]
+    });
+  }
+
+  goBack() {
+    this._location.back();
   }
 
 }
